124463f2b0e60aaafa4f87f8daed5230ba8720c1
20120447 <cuongparish@gmail.com>
2023-12-03 22:13:35 +0700

    fix UI sign-up

--- front-end/src/views/Signup.js
+++ front-end/src/views/Signup.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, InputGroup } from "react-bootstrap";
 import LeftBanner from "../components/LeftBanner.";
 import AuthService from "../service/auth.service";
 
 const Signup = () => {
   return (
-
     <Row className="landing-page vh-100 g-0">
-      <Col xs={4} className='left-content text-center vh-100'>
-          <LeftBanner />
+      <Col xs={4} className="left-content text-center vh-100">
+        <LeftBanner />
       </Col>
-      <Col xs={8} className='right-content vh-100'>
+      <Col xs={8} className="right-content vh-100">
         <Container className="navigator d-flex justify-content-between align-items-center">
-          <a href="/" className="button btn-back"><i class="fa fa-chevron-left" aria-hidden="true"></i> Return Home </a>
-          <span>Already a Member?<a href="/login" className="fw-bold"> LOGIN NOW </a></span>
+          <a href="/" className="button btn-back">
+            <i class="fa fa-chevron-left" aria-hidden="true"></i> Return Home{" "}
+          </a>
+          <span>
+            Already a Member?
+            <a href="/login" className="fw-bold">
+              {" "}
+              LOGIN NOW{" "}
+            </a>
+          </span>
         </Container>
         <Container className="text-center">
           <h3 className="mt-5 fw-bold">BECOME AN EXCLUSIVE MEMBERS</h3>
@@ -22,29 +29,49 @@ const Signup = () => {
             <Row className="justify-content-md-center">
               <Col lg={4}>
                 <InputGroup className="m-2 ml-4">
-                  <InputGroup.Text><i class="fa fa-user-o" aria-hidden="true"></i></InputGroup.Text>
-                  <Form.Control controlId="formBasicName" type="text" placeholder="Johnson Doe"/>
+                  <InputGroup.Text>
+                    <i class="fa fa-user-o" aria-hidden="true"></i>
+                  </InputGroup.Text>
+                  <Form.Control
+                    controlId="formBasicName"
+                    type="text"
+                    placeholder="Johnson Doe"
+                  />
                 </InputGroup>
               </Col>
               <Col lg={1}></Col>
               <Col lg={4}>
                 <InputGroup className="m-2">
-                  <InputGroup.Text><i class="fa fa-venus-mars" aria-hidden="true"></i></InputGroup.Text>
-                  <Form.Control controlId="formBasicGender" type="text" placeholder="Male"/>
+                  <InputGroup.Text>
+                    <i class="fa fa-venus-mars" aria-hidden="true"></i>
+                  </InputGroup.Text>
+                  <Form.Control
+                    controlId="formBasicGender"
+                    type="text"
+                    placeholder="Male"
+                  />
                 </InputGroup>
               </Col>
             </Row>
             <Row className="justify-content-md-center">
               <Col lg={4}>
                 <InputGroup className="m-2">
-                  <InputGroup.Text><i class="fa fa-user-o" aria-hidden="true"></i></InputGroup.Text>
-                  <Form.Control controlId="formBasicEmail" type="email" placeholder="example@email.com"/>
+                  <InputGroup.Text>
+                    <i class="fa fa-user-o" aria-hidden="true"></i>
+                  </InputGroup.Text>
+                  <Form.Control
+                    controlId="formBasicEmail"
+                    type="email"
+                    placeholder="example@email.com"
+                  />
                 </InputGroup>
               </Col>
               <Col lg={1}></Col>
               <Col lg={4}>
                 <InputGroup className="m-2">
-                  <InputGroup.Text><i class="fa fa-calendar" aria-hidden="true"></i></InputGroup.Text>
+                  <InputGroup.Text>
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                  </InputGroup.Text>
                   <Form.Control controlId="formBasicDOB" type="date" />
                 </InputGroup>
               </Col>
@@ -52,40 +79,60 @@ const Signup = () => {
             <Row className="justify-content-md-center">
               <Col lg={4}>
                 <InputGroup className="m-2">
-                  <InputGroup.Text><i class="fa fa-key" aria-hidden="true"></i></InputGroup.Text>
-                  <Form.Control controlId="formBasicPassword" type="password" placeholder="Password"/>
+                  <InputGroup.Text>
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                  </InputGroup.Text>
+                  <Form.Control
+                    controlId="formBasicPassword"
+                    type="password"
+                    placeholder="Password"
+                  />
                 </InputGroup>
               </Col>
               <Col lg={1}></Col>
               <Col lg={4}>
                 <InputGroup className="m-2">
-                  <InputGroup.Text><i class="fa fa-phone" aria-hidden="true"></i></InputGroup.Text>
-                  <Form.Control controlId="formBasicEmail" type="number" placeholder="0123456789"/>
+                  <InputGroup.Text>
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                  </InputGroup.Text>
+                  <Form.Control
+                    controlId="formBasicEmail"
+                    type="number"
+                    placeholder="0123456789"
+                  />
                 </InputGroup>
               </Col>
             </Row>
             <Row className="justify-content-md-center my-4">
               <Col lg={4}>
-                <Form.Group controlId="formBasicCheckbox" className='p-2'>
-                  <Form.Check type="checkbox" label="I Accept The Terms & Conditions" />
+                <Form.Group controlId="formBasicCheckbox" className="p-2">
+                  <Form.Check
+                    type="checkbox"
+                    label="I Accept The Terms & Conditions"
+                  />
                 </Form.Group>
               </Col>
             </Row>
-            <Button as={Col} lg={4} type="submit" className='bg-dark mb-2'>
+            <Button as={Col} lg={4} type="submit" className="bg-dark mb-2">
               Become a Member
             </Button>
           </Form>
 
-          <p>OR</p>
           <Row className="justify-content-md-center my-4">
-            <Button as={Col} lg={2} type="submit" className='bg-dark p-2' onClick={AuthService.googleAuth}>
-              Google
+            <Col lg={-1}></Col>
+            <Button as={Col} lg={1} type="submit" className="bg-dark p-3">
+              Verify
             </Button>
-            <Col lg={1}></Col>
-            <Button as={Col} lg={2} type="submit" className='bg-dark p-2' onClick={AuthService.facebookAuth}>
-              Facebook
-            </Button>
-            </Row>
+
+            <Col lg={4}>
+              <InputGroup className="m-2">
+                {/* <InputGroup.Text>
+                  <i class="fa fa-user-o" aria-hidden="true"></i>
+                </InputGroup.Text> */}
+                <Form.Control controlId="formBasicVerify" type="text" />
+              </InputGroup>
+            </Col>
+          </Row>
         </Container>
       </Col>
     </Row>
