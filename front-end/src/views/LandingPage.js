4afeccdba4521833d897e499106f79185a036d9c
20120447 <cuongparish@gmail.com>
2023-12-04 10:01:22 +0700

    fix UI LandingPage

--- front-end/src/views/LandingPage.js
+++ front-end/src/views/LandingPage.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
-import LeftBanner from '../components/LeftBanner.';
-import '../App.css';
+import React from "react";
+import { Row, Col, Image } from "react-bootstrap";
+import LeftBanner from "../components/LeftBanner.";
+import "../App.css";
 
 const LandingPage = () => {
   return (
     <Row className="landing-page vh-100 g-0">
-      <Col xs={4} className='left-content text-center vh-100'>
-          <LeftBanner />
+      <Col xs={4} className="left-content text-center vh-100">
+        <LeftBanner />
       </Col>
-      <Col xs={8} className='right-content text-center vh-100'>
-        <Image src={process.env.PUBLIC_URL + '/images/content.png'}
+      <Col xs={8} className="right-content text-center vh-100">
+        <Image
+          src={process.env.PUBLIC_URL + "/images/content.png"}
           className="d-inline-block"
           alt=""
           fluid
+          // style={{ maxHeight: "40px", maxWidth: "1000px" }}
         />
-        <div className='text-content fw-bold'>
-          <h1>MAKE YOUR GRADE SUMMARY EASIER AND FASTER</h1>
-          <p>The application supports easily restructuring scores, creating scoreboards, easily tracking scores, and quickly receiving appeal requests from students</p>
-          <a className="button btn-get-started" href='/login'>Get Started</a>
+        <div className="text-content fw-bold">
+          <h2 style={{ marginTop: "20px" }}>
+            MAKE YOUR GRADE SUMMARY EASIER AND FASTER
+          </h2>
+
+          <p1 style={{ marginTop: "20px" }}>
+            The application supports easily restructuring scores, creating
+            scoreboards, easily tracking scores, and quickly receiving appeal
+            requests from students
+          </p1>
+
+          <a
+            className="button btn-get-started "
+            href="/login"
+            style={{ marginTop: "20px" }}
+          >
+            Get Started
+          </a>
         </div>
       </Col>
     </Row>
