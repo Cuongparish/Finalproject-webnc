e0b04aa9de890f06a34fb48fcaf2b414751a3c9e
20120447 <cuongparish@gmail.com>
2023-12-04 21:05:39 +0700

    test

--- back-end/index.js
+++ back-end/index.js
@@ -23,13 +23,13 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(
-// 	cors({
-// 		origin: "http://localhost:3000",
-// 		methods: "GET,POST,PUT,DELETE",
-// 		credentials: true,
-// 	})
-// );
+app.use(
+  cors({
+    origin: "https://frontend-finalproject-webnc.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/user", accountRouter);
 app.use("/auth", authRouter);
