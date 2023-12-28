be22d704aca18168b5c38d62f56f976ab03ec666
20120447 <cuongparish@gmail.com>
2023-12-28 22:11:29 +0700

    fix format csv

--- back-end/models/grade.models.js
+++ back-end/models/grade.models.js
@@ -70,58 +70,6 @@ module.exports = {
     return { rows };
   },
 
-  // exporttoExcel_StudentList: async (req, res) => {
-  //   try {
-  //     const query = `SELECT user1."FullName", hs."StudentId"
-  //                   FROM "HocSinh" hs
-  //                   JOIN "HocSinhLopHoc" hslh ON hs."idHocSinh" = hslh."idHocSinh"
-  //                   JOIN "User" user1 ON hs."idUser" = user1."idUser"
-  //                   WHERE hslh."idLop" = $1`;
-
-  //     // const TenLop = await postgre.query(
-  //     //   `SELECT "TenLop" FROM "LopHoc" WHERE "idLop" = $1`,
-  //     //   [req.params.idLop]
-  //     // );
-
-  //     const { rows } = await postgre.query(query, [req.params.idLop]);
-
-  //     const workbook = new excel.Workbook();
-  //     const worksheet = workbook.addWorksheet("Sheet 1");
-
-  //     // Add headers to the worksheet
-  //     worksheet.columns = [
-  //       { header: "MSSV", key: "StudentId", width: 20 },
-  //       { header: "FullName", key: "FullName", width: 20 },
-  //     ];
-  //     if (rows && rows.length <= 0) {
-  //       return res.json({ msg: "Lớp học chưa có học sinh vào tham dự" });
-  //     }
-  //     // Add data to the worksheet
-  //     rows.forEach((row) => {
-  //       worksheet.addRow(row);
-  //     });
-  //     req.query.format;
-  //     // Set headers for the response
-  //     res.setHeader(
-  //       "Content-Type",
-  //       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  //     );
-  //     res.setHeader(
-  //       "Content-Disposition",
-  //       `attachment;filename=DanhSachHocSinh.xlsx`
-  //     );
-
-  //     // Pipe the workbook to the response
-  //     await workbook.xlsx.write(res);
-
-  //     // End the response
-  //     res.end();
-  //   } catch (error) {
-  //     console.error("Error exporting to Excel:", error);
-  //     res.status(500).send("Internal Server Error");
-  //   }
-  // },
-
   exporttoExcel_StudentList: async (req, res) => {
     try {
       const query = `SELECT user1."FullName", hs."StudentId"
@@ -176,7 +124,9 @@ module.exports = {
         );
 
         // Pipe the worksheet to the response as CSV
-        const csvStream = fastcsv.format({ headers: true });
+        const csvStream = fastcsv.format({
+          headers: ["StudentId", "FullName"],
+        });
         csvStream.pipe(res);
 
         rows.forEach((row) => {
