d3354225482166d96193697cd08e4dd6e752eed8
20120383 <nguyenductien71311@gmail.com>
2023-12-28 15:55:25 +0700

    Loader update 3

--- front-end/src/components/Loader.js
+++ front-end/src/components/Loader.js
@@ -5,12 +5,12 @@ const Loader = () => {
   return (
     <div className="loader" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
       </Spinner>
+      <h3 className="sr-only">Loading...</h3>
       {/* <Image
-          src={process.env.PUBLIC_URL + "Images/Spin-1s-200px.gif"}
-          alt="Loading..."
-        /> */}
+        src={process.env.PUBLIC_URL + "Images/Spin-1s-200px.gif"}
+        alt="Loading..."
+      /> */}
     </div>
   );
 };
