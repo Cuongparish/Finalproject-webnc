f575f975726e7e830d5fb40dd8b45cb782fa3ab3
20120383 <nguyenductien71311@gmail.com>
2023-12-28 15:48:19 +0700

    update loader 2

--- front-end/src/components/Loader.js
+++ front-end/src/components/Loader.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
 
 const Loader = () => {
   return (
     <div className="loader" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      {/* <Spinner animation="border" role="status">
+      <Spinner animation="border" role="status">
         <span className="sr-only">Loading...</span>
-      </Spinner> */}
-      <Image
+      </Spinner>
+      {/* <Image
           src={process.env.PUBLIC_URL + "Images/Spin-1s-200px.gif"}
           alt="Loading..."
-        />
+        /> */}
     </div>
   );
 };
