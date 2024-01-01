02b80875c8dca65e9dd05cb978565960bb8a3caf
20120447 <cuongparish@gmail.com>
2024-01-01 16:31:03 +0700

    change db

--- back-end/models/database.js
+++ back-end/models/database.js
@@ -2,18 +2,18 @@ const { Pool } = require("pg");
 
 require("dotenv").config();
 
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-// });
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
 
 //Cua cuong
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "final",
-  password: "123456",
-  port: 5432,
-});
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "final",
+//   password: "123456",
+//   port: 5432,
+// });
 
 // Cua tien
 // const pool = new Pool({
