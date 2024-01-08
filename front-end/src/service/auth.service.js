b9df5d72ee7f2ea2a8ae11d98d2ac0fb629ddbe7
20120447 <cuongparish@gmail.com>
2024-01-08 14:09:57 +0700

    change API_URL

--- front-end/src/service/auth.service.js
+++ front-end/src/service/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "https://finalproject-webnc.vercel.app";
-const API_URL = "http://localhost:5000";
+const API_URL = "https://finalproject-webnc.vercel.app";
+// const API_URL = "http://localhost:5000";
 
 const googleAuth = () => {
   window.open(`${API_URL}/auth/google/callback`, "_self");
